92b91d22405fbac1e29c96abef17c0f80641c7fb
Mateo <contreras4893@gmail.com>
2020-12-02 16:27:17 -0600

    optimizacion de pruebas, configuracion firebase

--- test/cypress/integration/login_registro.spec.js
+++ test/cypress/integration/login_registro.spec.js
@@ -1,38 +1,40 @@
 'use stric'
 
 describe('Pruebas de login',() => {
-    it('Debe cargar el login', () => {
+
+    afterEach(() => {
         cy.visit('/login')
     })
 
+    it('Debe cargar el login', () => {
+        
+    })
+
     it('Debe registrar un usuario', () => {
-        cy.visit('/login')
         cy.contains('Crear una cuenta').click()
         cy.get('#name').type('Usuario')
         cy.get('#title').type('Prueba')
         cy.get('#email2').type('test@test.com')
         cy.get('#password2').type('test1234')
         cy.contains('.button', 'Registrarse').click()
-        cy.wait(500)
+        cy.wait(2000)
         //cy.contains('.error-msg').should('not.exist')
         cy.get('.error-msg').should('not.be.visible')
     })
 
     it('No debe registrar un usuario existente', () => {
-        cy.visit('/login')
         cy.contains('Crear una cuenta').click()
         cy.get('#name').type('Usuario')
         cy.get('#title').type('Prueba')
         cy.get('#email2').type('test@test.com')
         cy.get('#password2').type('test1234')
         cy.contains('.button', 'Registrarse').click()
-        cy.wait(500)
+        cy.wait(1000)
         //cy.contains('.error-msg').should('not.exist')
         cy.get('.error-msg').should('be.visible')
     })
 
     it('Debe logear un usuario', () => {
-        cy.visit('/login')
         cy.get('#email1').type('test@test.com')
         cy.get('#password1').type('test1234')
         cy.contains('.button', 'Ingresar').click()
@@ -40,7 +42,6 @@ describe('Pruebas de login',() => {
     })
 
     it('Debe fallar con un usuario erroneo', () => {
-        cy.visit('/login')
         cy.get('#email1').type('fail@test.com')
         cy.get('#password1').type('test1234')
         cy.contains(".button", 'Ingresar').click()
@@ -49,7 +50,6 @@ describe('Pruebas de login',() => {
     })
 
     it('Debe fallar con una contraseña erronea', () => {
-        cy.visit('/login')
         cy.get('#email1').type('test@test.com')
         cy.get('#password1').type('T3st1236')
         cy.contains(".button", 'Ingresar').click()
